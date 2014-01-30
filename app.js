207d011ee0b8446d3aa532b2017e9a6343568566
cl-aaron <aaron.elligsen@creativelive.com>
2014-01-29 17:10:30 -0800

    handle stage and prod
    closes #9

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 process.chdir(__dirname);
-if(process.env.NODE_ENV && process.env.NODE_ENV === "prod") {
+if(process.env.NODE_ENV && (process.env.NODE_ENV === "prod" || process.env.NODE_ENV === "stage")) {
   process.env.NODE_ENV = "production";
 }else{
   process.env.NODE_ENV = "development";
